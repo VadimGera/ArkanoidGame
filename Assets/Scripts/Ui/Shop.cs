604cc1e364a0515ae50f413c7f7c8fe626d6efd2
VadimGera <vadimnvidia@gmail.com>
2023-04-06 18:05:29 +0300

    осталось доделать рандомные значения в окнах магазина

---
+++ Assets/Scripts/Ui/Shop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.Ui
+{
+    public class Shop : MonoBehaviour
+    {
+        [SerializeField] private GameObject _itemPrefab; 
+        [SerializeField] public RectTransform _content; 
+        public int minItems = 5; 
+        public int maxItems = 20;
+
+        void Start()
+        {
+            int numItems = Random.Range(minItems, maxItems + 1);
+            
+            for (int i = 0; i < numItems; i++)
+            {
+                GameObject item = Instantiate(_itemPrefab, _content);
+
+                
+
+                
+            }
+        }
+    }
+}
